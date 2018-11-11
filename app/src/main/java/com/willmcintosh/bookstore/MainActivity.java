fa3d3a8d16def2be84d881169ebde83d4790f78a
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-11-11 18:01:14 -0500

    set up BookContract and BookDbHelper

--- app/src/main/java/com/willmcintosh/bookstore/MainActivity.java
+++ app/src/main/java/com/willmcintosh/bookstore/MainActivity.java
@@ -1,13 +1,33 @@
 package com.willmcintosh.bookstore;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    // database helper
+    private BookDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // For accessing database
+        mDbHelper = new BookDbHelper(this);
     }
 }
---
+++ app/src/main/java/com/willmcintosh/bookstore/data/BookContract.java
@@ -0,0 +1,48 @@
+package com.willmcintosh.bookstore.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * API Contract for Bookstore app
+ */
+public final class BookContract {
+
+    private BookContract() {}
+
+
+    public static final class BookEntry implements BaseColumns {
+        public final static String TABLE_NAME = "books";
+
+        /**
+         * Unique ID for the book
+         */
+        public static final String _ID = BaseColumns._ID;
+
+        /**
+         * Product name
+         */
+        public static final String COLUMN_PRODUCT_NAME = "product_name";
+
+        /**
+         * Price
+         */
+        public static final String COLUMN_PRICE = "price";
+
+        /**
+         * Quantity
+         */
+        public static final String COLUMN_QUANTITY = "quantity";
+
+        /**
+         * Supplier Name
+         */
+        public static final String COLUMN_SUPPLIER_NAME = "supplier_name";
+
+        /**
+         * Supplier Phone Number
+         */
+        public static final String COLUMN_SUPPLIER_PHONE = "supplier_phone";
+
+
+    }
+}
