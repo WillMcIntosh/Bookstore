f0da1e8702f3b55202d6fb6a49b0c6a2e9f1c94c
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-11-11 20:38:06 -0500

    added insert and delete dummy data

--- app/src/main/java/com/willmcintosh/bookstore/EditorActivity.java
+++ app/src/main/java/com/willmcintosh/bookstore/EditorActivity.java
@@ -3,4 +3,5 @@ package com.willmcintosh.bookstore;
 import android.support.v7.app.AppCompatActivity;
 
 public class EditorActivity extends AppCompatActivity {
+    //
 }
--- app/src/main/java/com/willmcintosh/bookstore/MainActivity.java
+++ app/src/main/java/com/willmcintosh/bookstore/MainActivity.java
@@ -1,15 +1,17 @@
 package com.willmcintosh.bookstore;
 
-import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.service.autofill.TextValueSanitizer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.willmcintosh.bookstore.data.BookContract.BookEntry;
 import com.willmcintosh.bookstore.data.BookDbHelper;
@@ -77,12 +79,12 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             // Create a header row in the text view
-            displayView.setText("The books table contains " + cursor.getCount() + " bookss.\n\n");
-            displayView.append(BookEntry._ID + " - " +
-                    BookEntry.COLUMN_PRODUCT_NAME + " - " +
-                    BookEntry.COLUMN_PRICE + " - " +
-                    BookEntry.COLUMN_QUANTITY + " - " +
-                    BookEntry.COLUMN_SUPPLIER_NAME + " - " +
+            displayView.setText("The books table contains " + cursor.getCount() + " books.\n\n");
+            displayView.append(BookEntry._ID + " | " +
+                    BookEntry.COLUMN_PRODUCT_NAME + " | " +
+                    BookEntry.COLUMN_PRICE + " | " +
+                    BookEntry.COLUMN_QUANTITY + " | " +
+                    BookEntry.COLUMN_SUPPLIER_NAME + " | " +
                     BookEntry.COLUMN_SUPPLIER_PHONE + "\n");
 
             // get index of each column
@@ -102,11 +104,11 @@ public class MainActivity extends AppCompatActivity {
                 String currentSupplier = cursor.getString(supplierColumnIndex);
                 String currentPhone = cursor.getString(phoneColumnIndex);
                 // Display current values in TextView
-                displayView.append(("\n" + currentID + " - " +
-                        currentProductName + " - " +
-                        currentPrice + " - " +
-                        currentQuantity + " - " +
-                        currentSupplier + " - " +
+                displayView.append(("\n" + currentID + " | " +
+                        currentProductName + " | " +
+                        currentPrice + " | " +
+                        currentQuantity + " | " +
+                        currentSupplier + " | " +
                         currentPhone));
             }
 
@@ -116,4 +118,74 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    /**
+     * Insert dummy data to test functionality for part 1
+     */
+    private void insertBook() {
+        // get db in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // create ContentValues object
+        ContentValues values = new ContentValues();
+        values.put(BookEntry.COLUMN_PRODUCT_NAME, "Test Book");
+        values.put(BookEntry.COLUMN_PRICE, 500);
+        values.put(BookEntry.COLUMN_QUANTITY, 4);
+        values.put(BookEntry.COLUMN_SUPPLIER_NAME, "Random House");
+        values.put(BookEntry.COLUMN_SUPPLIER_PHONE, "+1-888-555-1234");
+
+        long newRowId = db.insert(BookEntry.TABLE_NAME, null, values);
+
+        // Show a toast message depending on whether or not the insertion was successful
+        if (newRowId == -1) {
+            // If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, "Error with importing dummy data", Toast.LENGTH_SHORT).show();
+        } else {
+            // Otherwise, the insertion was successful and we can display a toast with the row ID.
+            Toast.makeText(this, "Book saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    /**
+     * Delete all entries from the books table
+     */
+    private void deleteBooks() {
+        //  get db in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        long rowsDeleted = db.delete(BookEntry.TABLE_NAME, "1", null);
+
+        // Show a toast message for number of rows deleted
+        Toast.makeText(this, "Successfully deleted " + rowsDeleted + " rows.", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_main file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                insertBook();
+                displayDatabaseInfo();
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                deleteBooks();
+                displayDatabaseInfo();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
