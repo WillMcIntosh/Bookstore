a06a979472ee610368dc55bbed309d50625a4c6f
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-11-12 19:44:40 -0500

    updated book contract with URIs

--- app/src/main/java/com/willmcintosh/bookstore/data/BookContract.java
+++ app/src/main/java/com/willmcintosh/bookstore/data/BookContract.java
@@ -1,5 +1,6 @@
 package com.willmcintosh.bookstore.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -10,9 +11,21 @@ public final class BookContract {
     private BookContract() {
     }
 
+    /**
+     * Create Content URI
+     */
+    public static final String CONTENT_AUTHORITY = "com.willmcintosh.bookstore";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_BOOKS = "books";
+
 
     public static final class BookEntry implements BaseColumns {
         public final static String TABLE_NAME = "books";
+
+        /** Content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_BOOKS);
 
         /**
          * Unique ID for the book
