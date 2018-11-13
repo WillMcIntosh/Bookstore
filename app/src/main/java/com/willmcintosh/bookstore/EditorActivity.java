331833785a077cc0e3fad67bd89b45af08b8627f
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-11-12 20:20:37 -0500

    added layout and methods for editor activity similar to pets app.

--- app/src/main/java/com/willmcintosh/bookstore/EditorActivity.java
+++ app/src/main/java/com/willmcintosh/bookstore/EditorActivity.java
@@ -1,6 +1,123 @@
 package com.willmcintosh.bookstore;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.willmcintosh.bookstore.data.BookDbHelper;
+import com.willmcintosh.bookstore.data.BookContract.BookEntry;
 
 public class EditorActivity extends AppCompatActivity {
+
+    /** EditText field to enter the book's name */
+    private EditText mNameEditText;
+
+    /** EditText field to enter the book's price */
+    private EditText mPriceEditText;
+
+    /** EditText field to enter the book's quantity */
+    private EditText mQuantityEditText;
+
+    /** EditText field to enter the supplier's name */
+    private EditText mSupplierEditText;
+
+    /** EditText field to enter the supplier's phone */
+    private EditText mPhoneEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        // Find all relevant views that we will need to read user input from
+        mNameEditText = findViewById(R.id.edit_book_name);
+        mPriceEditText = findViewById(R.id.edit_book_price);
+        mQuantityEditText = findViewById(R.id.edit_book_quantity);
+        mSupplierEditText = findViewById(R.id.edit_supplier_name);
+        mPhoneEditText = findViewById(R.id.edit_supplier_phone);
+
+
+    }
+
+    /**
+     * Get user input from editor and save new book into database
+     */
+    private void insertBook() {
+        // Read from input fields
+        String nameString = mNameEditText.getText().toString().trim();
+        String priceString = mPriceEditText.getText().toString().trim();
+        int price = Integer.parseInt(priceString);
+        String quantityString = mQuantityEditText.getText().toString().trim();
+        int quantity = Integer.parseInt(quantityString);
+        String supplierNameString = mSupplierEditText.getText().toString().trim();
+        String phoneString = mPhoneEditText.getText().toString().trim();
+
+        // Create database helper
+        BookDbHelper mDbHelper = new BookDbHelper(this);
+        // get db in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // create ContentValues object
+        ContentValues values = new ContentValues();
+        values.put(BookEntry.COLUMN_PRODUCT_NAME, nameString);
+        values.put(BookEntry.COLUMN_PRICE, price);
+        values.put(BookEntry.COLUMN_QUANTITY, quantity);
+        values.put(BookEntry.COLUMN_SUPPLIER_NAME, supplierNameString);
+        values.put(BookEntry.COLUMN_SUPPLIER_PHONE, phoneString);
+
+        long newRowId = db.insert(BookEntry.TABLE_NAME, null, values);
+
+        // Show a toast message depending on whether or not the insertion was
+        // successful
+        if (newRowId == -1) {
+            // If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, "Error with importing dummy data", Toast
+                    .LENGTH_SHORT).show();
+        } else {
+            // Otherwise, the insertion was successful and we can display a
+            // toast with the row ID.
+            Toast.makeText(this, "Book saved with row id: " + newRowId, Toast
+                    .LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                // Save pet to database
+                insertBook();
+                // Exit activity
+                finish();
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (CatalogActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
